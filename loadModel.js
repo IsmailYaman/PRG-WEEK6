b1b6eb6bdaf8276a81ca08d3a0aaef11366f36ea
Ismail <iso_yaman@outlook.com>
2023-03-26 17:03:19 +0200

    Added styling

--- loadModel.js
+++ loadModel.js
@@ -1,22 +1,40 @@
 import { DecisionTree } from "./libraries/decisiontree.js"
 import { VegaTree } from "./libraries/vegatree.js"
 
-function loadSavedModel() {
+const mushroomClass = document.getElementById("mushroomClass");
+const mushroomClassButton = document.getElementById("mushroomClassButton");
+const userInput = document.getElementById("userInput");
+
+
+mushroomClassButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const value = mushroomClass.value || "please enter E or P";
+  loadSavedModel(value)
+})
+
+function loadSavedModel(value) {
     fetch("./model.json")
         .then((response) => response.json())
-        .then((model) => modelLoaded(model))
+        .then((model) => modelLoaded(model, value))
 }
 
-function modelLoaded(model) {
+function modelLoaded(model, value) {
     console.log(model)
-    let decisionTree = new DecisionTree(model)
+    const decisionTree = new DecisionTree(model)
 
     // test om te zien of het werkt
-
-    let prediction = decisionTree.predict(mushroom)
+    const mushroom = { class: value}
+    const prediction = decisionTree.predict(mushroom)
     console.log("predicted " + prediction)
 
+    if(value !== undefined){
+        userInput.innerText = `Your personal prediction was: ${value}. The computer prediction is: ${prediction}`;
+    }
+
+
     let visual = new VegaTree('#view', 800, 400, decisionTree.toJSON())
+
+
 
 }
 
