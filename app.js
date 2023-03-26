694e94138bf29bc5c2029915a2be636733d97928
Ismail <iso_yaman@outlook.com>
2023-03-26 18:03:11 +0200

    Code formatting

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-import { DecisionTree } from "./libraries/decisiontree.js"
-import { VegaTree } from "./libraries/vegatree.js"
+import {
+    DecisionTree
+} from "./libraries/decisiontree.js"
+import {
+    VegaTree
+} from "./libraries/vegatree.js"
 
 //
 // DATA
 //
 const csvFile = "data/mushrooms.csv"
-const trainingLabel = "class"  
-const ignored = ["cap-shape","cap-surface","cap-color","bruises","odor","gill-color","population","habitat"]  
+const trainingLabel = "class"
+const ignored = ["cap-shape", "cap-surface", "cap-color", "bruises", "odor", "gill-color", "population", "habitat"]
 
 //
 // laad csv data als json
@@ -16,7 +20,7 @@ function loadData() {
         download: true,
         header: true,
         dynamicTyping: true,
-        complete: results => trainModel(results.data)   // gebruik deze data om te trainen
+        complete: results => trainModel(results.data) // gebruik deze data om te trainen
     })
 }
 
@@ -26,7 +30,7 @@ function loadData() {
 function trainModel(data) {
     // todo : splits data in traindata en testdata
     data.sort(() => (Math.random() - 0.5))
-    
+
     let trainData = data.slice(0, Math.floor(data.length * 0.8))
     let testData = data.slice(Math.floor(data.length * 0.8) + 1)
 
@@ -42,14 +46,6 @@ function trainModel(data) {
     // Teken de boomstructuur - DOM element, breedte, hoogte, decision tree
     let visual = new VegaTree('#view', 800, 400, decisionTree.toJSON())
 
-
-    // todo : maak een prediction met een sample uit de testdata
-    // let mushroom = testData[0]
-    // let mushroomPrediction = decisionTree.predict(mushroom)
-    // console.log(mushroom)
-    // console.log(mushroomPrediction)
-    // console.log(`Which is edible : ${mushroomPrediction}`)
-
     // todo : bereken de accuracy met behulp van alle test data
     let amountCorrect = 0
     let totalAmount = testData.length
@@ -57,19 +53,16 @@ function trainModel(data) {
     let trueFalse = 0
     let falseTrue = 0
     let falseFalse = 0
-    
-    for (let i = 0; i < testData.length; i++){
+
+    for (let i = 0; i < testData.length; i++) {
         let message = testAccuracy(testData[i], decisionTree)
-        if(message === "goed voorspeld edible!"){
+        if (message === "goed voorspeld edible!") {
             trueTrue++
-        }
-        else if(message === "goed voorspeld poisoned!"){
+        } else if (message === "goed voorspeld poisoned!") {
             falseFalse++
-        }
-        else if(message === "fout voorspeld edible!"){
+        } else if (message === "fout voorspeld edible!") {
             falseTrue++
-        }
-        else if(message === "fout voorspeld poisoned!"){
+        } else if (message === "fout voorspeld poisoned!") {
             trueFalse++
         }
     }
@@ -89,7 +82,9 @@ function trainModel(data) {
 
 function testAccuracy(mushroom, decisionTree) {
     // kopie van passenger maken, zonder het "survived" label
-    const mushroomsWithOutClass = { ...mushroom }
+    const mushroomsWithOutClass = {
+        ...mushroom
+    }
     delete mushroomsWithOutClass.class
 
     // prediction
@@ -98,17 +93,17 @@ function testAccuracy(mushroom, decisionTree) {
     // console.log(mushroom.class)
     // vergelijk de prediction met het echte label
     let message
-    if(prediction == "e" && mushroom.class == "p") {
+    if (prediction == "e" && mushroom.class == "p") {
         message = "fout voorspeld edible!"
     }
-    if(prediction == "e" && mushroom.class == "e") {
+    if (prediction == "e" && mushroom.class == "e") {
         message = "goed voorspeld edible!"
     }
-    if(prediction == "p" && mushroom.class == "e") {
-        message = "fout voorspeld poisoned!"
+    if (prediction == "p" && mushroom.class == "e") {
+        message = "fout voorspeld poisonous!"
     }
-    if(prediction == "p" && mushroom.class == "p") {
-        message = "goed voorspeld poisoned!"
+    if (prediction == "p" && mushroom.class == "p") {
+        message = "goed voorspeld poisonous!"
     }
 
     return message
--- loadModel.js
+++ loadModel.js
@@ -1,5 +1,9 @@
-import { DecisionTree } from "./libraries/decisiontree.js"
-import { VegaTree } from "./libraries/vegatree.js"
+import {
+    DecisionTree
+} from "./libraries/decisiontree.js"
+import {
+    VegaTree
+} from "./libraries/vegatree.js"
 
 const mushroomClass = document.getElementById("mushroomClass");
 const mushroomClassButton = document.getElementById("mushroomClassButton");
@@ -7,9 +11,9 @@ const userInput = document.getElementById("userInput");
 
 
 mushroomClassButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  const value = mushroomClass.value || "please enter E or P";
-  loadSavedModel(value)
+    e.preventDefault();
+    const value = mushroomClass.value || "please enter E or P";
+    loadSavedModel(value)
 })
 
 function loadSavedModel(value) {
@@ -23,18 +27,17 @@ function modelLoaded(model, value) {
     const decisionTree = new DecisionTree(model)
 
     // test om te zien of het werkt
-    const mushroom = { class: value}
+    const mushroom = {
+        class: value
+    }
     const prediction = decisionTree.predict(mushroom)
     console.log("predicted " + prediction)
 
-    if(value !== undefined){
+    if (value !== undefined) {
         userInput.innerText = `Your personal prediction was: ${value}. The computer prediction is: ${prediction}`;
     }
 
-
     let visual = new VegaTree('#view', 800, 400, decisionTree.toJSON())
-
-
 
 }
 
